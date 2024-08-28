5a35b7d483d0ededbb8dac8c913a0cccb57a817a
logiee2001 <logeeshwari@student.tce.edu>
2024-08-28 23:19:45 +0530

    first commit

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "../components/Projects/ProjectCard";
+import Particle from "../components/Particle";
+import pg from "../assets/projects/pg.png";
+import project from "../assets/projects/project.jpeg";
+import lift from "../assets/projects/lift.png";
+import kickstart from "../assets/projects/kickstart.png";
+
+const Projects = () => {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          Recent Top <strong className="yellow">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={lift}
+              isBlog={false}
+              title="Storyverse"
+              description="This app is for the readers like us. In this app we can talk to the author of the novel also rate them .we can also write a novel and publish it"
+              ghLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={project}
+              isBlog={false}
+              title="Women wealth chatbot"
+              description="This is a bot that answers your questions like your health issues or any law that you want to know"
+              ghLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={kickstart}
+              isBlog={false}
+              title="PATTRACK"
+              description="This project will say the disease with the help of the symptoms . I later developed it as a app"
+              ghLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={pg}
+              isBlog={false}
+              title="Service provider application using appypie "
+              description="An application to which enables you to find the service that you desire for based on your location."
+              ghLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={pg}
+              isBlog={false}
+              title="Simple React Js application "
+              description="An application to which enables you to find the service that you desire for based on your location."
+              ghLink=""
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  )
+}
+
+export default Projects
